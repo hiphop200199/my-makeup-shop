1cf83661a6f06b8da02f75471470f8dd59a1b8e1
Eric Tsai <hiphop200199@gmail.com>
2023-01-08 20:49:50 +0800

    專案react化

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>
+        <header>
+        <div className="logo">shanel</div>
+        </header>
+        <div className="box">
+            <h1>Lorem, ipsum.</h1>
+            <h2>Lorem ipsum dolor sit amet.</h2>
+            <img className="pic" src="" alt=""/>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias debitis eius, accusamus facere sit ab aliquam, eos quasi dicta suscipit nam accusantium dolorum. Porro voluptates veniam ad? Quisquam nisi alias repellendus inventore, itaque minus voluptatum quLorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque nulla ullam la
+            </p>
+            <p>
+                Animi quod consequuntur non aspernatur saepe, a pquaerat, recusandae fugiat omnis tempora cumque veniam possimus quisquam repellendus!
+            </p>
+                <img class="pic2" src="" alt=""/>
+                <p>
+                 Cumque aliquid ab ratione rem, provident officiis magnam  mmagni facere quod mollitia perferendis ipsam. Voluptas assumenda inventore voluptatibus, in esse quaerat atque!
+                </p>
+                
+        </div>
+        <footer>
+            <h3>copyright...all rights reserved</h3>
+        </footer>
+    
+    </div>
+  )
+}
+
+export default Home
